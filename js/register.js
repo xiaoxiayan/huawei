9aa4582f9b11727bfe0e5f79fb3a13fd29c5edf1
LLLQW <a690150618@qq.com>
2018-10-11 20:40:36 +0800

    正则

--- js/register.js
+++ js/register.js
@@ -1,5 +1,25 @@
 $(function(){
 	$(".SCcenter").click(function(){
-		
+		$(".change").toggle("block")
+	})
+	$(".change").on("click","li", function(){
+		$(".SCcenter span").val($("li").val())
+	})
+	var reg = /^1(3|4|5|7|8)\d{9}$/
+	var reg_mima =/^[a-z]$/
+	$("#phone").keydown(function(){
+		$(".phoneover").css("display","none")
+	})
+	$(".zc").click(function(){
+		if(reg.test($("#phone").val())){
+			$(".phoneover").css("display","block")
+		}
+	})
+	$("#password2").blur(function(){
+		if($("#password").val() != $("#password2").val()){
+			$(".passover").css("display","block")
+		}else{
+			$(".passover").css("display","none")
+		}
 	})
 })
