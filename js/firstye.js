5736bf18497e4055aefd69fe5de05a7af65f6b51
LLLQW <a690150618@qq.com>
2018-10-18 12:07:32 +0800

    往左右

--- js/firstye.js
+++ js/firstye.js
@@ -50,65 +50,67 @@
 	fade.init()
 //	控制精彩推荐
 $(".JPshow").mouseenter(function(event){
-	var width =event.target.parentElement.children.length;
-	var $long = width *217;
-	console.log($long)
-	var target = event.target.parentElement
-	console.log(target)
-	var _this = $(this)
-	$('.JPboxR').on("click" ,function(){
-		var left =Math.abs(parseInt($(".JPshow").css("left")))+1200
-		if(left<$long){
-			$(".JPshow").animate({left:'-=1200px'},'fast',function(){
-			if(left>$long-1200){
-			$(".JPshow").css("left",(-$long+1250))
-			$('.JPboxR').css("background","#03267A")
-			}	
-			});
-		}
-	})
+	var width =$(this).closest('.JPshow').children().length;
+	var $MAX = (width *217);
+	console.log($MAX)
+	$(this).siblings('.JPboxR').click(function(){
+//		设置初始位置值
+		let le =Math.abs(parseInt($(this).siblings('.JPshow').css("left")))+1200;
+//		设置可移动距离
+		let maxMove = $MAX-le;
+				console.log('可以移动的值'+maxMove,"当前移动的值"+le)
+			if(maxMove>1200){
+				$(this).siblings('.JPshow').animate({left:'-=1200'},'fast')
+			}
+			if(maxMove<1200){
+				$(this).siblings('.JPshow').animate({left:`-=${maxMove}px`},'fast')
+				
+			}
+//			$(this).css("background","#03267A")
+			})	
+			
+	
 })
 //    自动小轮播
-//var fade2 = function(){
-//		$dtBox = $('.LBT_dian');
-//		$igBox = $('.LBT_IMG');
-//		var timer;
-//		return {
-//			init(){
-//				this.events();
-//				this.autoPlay(0);
-//			},
-//			showImg(index){
-//				$dtBox.children().removeClass();
-//				$dtBox.children().eq(index).addClass('current');
-//				$igBox.children('li').eq(index).fadeIn().siblings().fadeOut();
-//			},
-//			autoPlay(index){
-//				var _this = this;
-//				clearInterval(timer);
-//				timer = setInterval(function(){
-//					index++;
-//					if (index==$('.LBT_dian li').length) {
-//						index = 0;
-//					}
-//					_this.showImg(index);
-//				},2500);
-//			},
-//			events(){
-//				var _this = this;
-//				$dtBox.find('li').on('mouseenter',function(){
-//					clearInterval(timer);
-//					_this.showImg($(this).index());
-////					_this.autoPlay($(this).index());
-//				});
-//				$(".Lbbroung").on('mouseenter',function(){
-//					console.log(11)
-//					clearInterval(timer);
-//				});
-//				$(".Lbbroung").on('mouseleave',function(){
-//					console.log(112)
-//						_this.autoPlay($(this).index());
-//				});
-//			}
-//		}
-//		}
+var fade2 =(function(){
+		$dtBox = $('.LBT_dian');
+		$igBox = $('.LBT_IMG');
+		var timer;
+		return {
+			init(){
+				this.events();
+				this.autoPlay(0);
+			},
+			showImg(index){
+				$dtBox.children().removeClass();
+				$dtBox.children().eq(index).addClass('current');
+				$igBox.children('li').eq(index).fadeIn().siblings().fadeOut();
+			},
+			autoPlay(index){
+				var _this = this;
+				clearInterval(timer);
+				timer = setInterval(function(){
+					index++;
+					if (index==$('.LBT_dian li').length) {
+						index = 0;
+					}
+					_this.showImg(index);
+				},2500);
+			},
+			events(){
+				var _this = this;
+				$dtBox.find('li').on('mouseenter',function(){
+					clearInterval(timer);
+					_this.showImg($(this).index());
+//					_this.autoPlay($(this).index());
+				});
+				$(".Lbbroung").on('mouseenter',function(){
+					clearInterval(timer);
+				});
+				$(".Lbbroung").on('mouseleave',function(){
+						_this.autoPlay($(this).index());
+				});
+			}
+		}
+		}())
+	fade2.init()
